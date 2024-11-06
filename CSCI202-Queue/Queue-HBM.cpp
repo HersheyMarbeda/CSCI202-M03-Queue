1c2e7a85b748006596e58e4eaaadeda3f9bb7839
Hershey Marbeda <marbedahershey@gmail.com>
2024-11-06 15:30:19 -0500

    Added some comments

--- CSCI202-Queue/Queue-HBM.cpp
+++ CSCI202-Queue/Queue-HBM.cpp
@@ -10,6 +10,12 @@
  *                                                          *
  *        Description: This program demonstrates the use of queues                    *
  **************************************************************/
+/*
+   Programming Exercise #13 Ch. 17 (p. 1277) - This chapter describes the array implementation of queues that use a special array slot,
+   called the reserved slot, to distinguish between an empty and a full queue. Write the definition of the class and the definitions
+   of the function members of this queue design. Also, write a test program to test various operations on a queue
+*/
+
 
 #include <iostream>
 #include <queue>
---
+++ README.md
@@ -0,0 +1,8 @@
+# CSCI 202: Programming Assignment
+## Assignment Description
+- The students were required to implemenet Queues and Hash Functions.
+### My Methods
+1. Queues:
+   - I implemented Queues using singly Linked Lists and in FIFO structure
+2. Hash Functions
+   - [I haven't implemented this yet.]
